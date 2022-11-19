8ab9e58a2c166cda835ce0a9783ddb39bf15add8
Amel Ben <samia.ben@disroot.org>
2022-11-19 18:34:56 +0100

    update

--- script.R
+++ script.R
@@ -5,23 +5,28 @@ macacaIndex <- vector()
 rattusIndex <- vector()
 musIndex <- vector()
 
-mrcaSinge <- vector()
-mrcaSouris <- vector()
+mrcaApe <- vector()
+mrcaMouse <- vector()
 mrcaCommon <- vector()
 
 files <- list.files(path="./trees", pattern="*.rootree", full.names=TRUE, recursive=FALSE)
 lapply(files, function(x) {
 				 tree <- read.tree(x)
-
+        print(tree)
 				 homoIndex <<- append(homoIndex, which(tree$tip.label=="Homo_sapiens")) 
 				 macacaIndex <<- append(macacaIndex, which(tree$tip.label=="Macaca_fascicularis"))
 				 rattusIndex <<- append(rattusIndex, which(tree$tip.label=="Rattus_norvegicus"))
-				 rattusIndex <<- append(musIndex, which(tree$tip.label=="Mus_musculus"))
+				 musIndex <<- append(musIndex, which(tree$tip.label=="Mus_musculus"))
 
-				 mrcaSinge <<- getMRCA(tree, c("Homo_sapiens", "Macaca_fascicularis"))
-				 mrcaSouris <<- getMRCA(tree, c("Rattus_norvegicus", "Mus_musculus"))
+				 mrcaApe <<- getMRCA(tree, c("Homo_sapiens", "Macaca_fascicularis"))
+				 mrcaMouse <<- getMRCA(tree, c("Rattus_norvegicus", "Mus_musculus"))
 
-				 mrcaCommon <<- getMRCA(tree, c(mrcaSinge, mrcaSouris))
-				 dist <- dist.nodes(tree)[mrcaCommon, mrcaSouris] 
+				 mrcaCommon <<- getMRCA(tree, c(mrcaApe, mrcaMouse))
+				 distCM <- dist.nodes(tree)[mrcaCommon, mrcaMouse]
+				 distCA <- dist.nodes(tree)[mrcaCommon, mrcaApe]
+				 distCh <- dist.nodes(tree)[mrcaCommon, homoIndex]
+				 distCma <- dist.nodes(tree)[mrcaCommon, macacaIndex]
+				 distCr <- dist.nodes(tree)[mrcaCommon, rattusIndex]
+				 distCmu <- dist.nodes(tree)[mrcaCommon, musIndex]
 })
-
+print(rattusIndex)
